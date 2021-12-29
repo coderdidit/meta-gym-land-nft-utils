30e2d7357a10ca400a22137fb748047041afcfdf
mkuzdowicz <martin.kuzdowicz@gmail.com>
2021-12-29 14:15:50 +0000

    metadata mapping

--- metadata.js
+++ metadata.js
@@ -10,17 +10,23 @@ const imgCount = 4;
 const hash = "123"
 
 const metadataMapping = new Map([
-    [1, {
-        name: "",
-        description: ""
+    [1, { // blue
+        name: "Erach Salte",
+        description: "Want's to go to the gym to boost energy and stay healthy",
+        background_color: "5E75C6",
+        attributes: {},
     }],
-    [2, {
-        name: "",
-        description: ""
+    [2, { // girl
+        name: "Orlai Grande",
+        description: "Want's to go to the gym to boost energy and look beautiful",
+        background_color: "F2D8D6",
+        attributes: {},
     }],
-    [3, {
-        name: "",
-        description: ""
+    [3, { // red
+        name: "Odialt Drutum",
+        description: "Want's to go to the gym to improve his muscles and look good",
+        background_color: "F2EBD1",
+        attributes: {},
     }]
 ]);
 
@@ -31,6 +37,14 @@ for (let i = 1; i < imgCount; i++) {
         path: outPath,
         content: {
             image: `ipfs://${hash}/images/${i}.png`,
+            cover_image: `ipfs://${hash}/images/${i}.png`,
+            collection_name: "",
+            collection_cover_image: `ipfs://${hash}/images/cover.gif`,
+            sprite: {
+                image: `ipfs://${hash}/images/${i}.png`,
+                json: {}
+            },
+            category: "avatars",
             ...metadataMapping.get(i)
         }
     })
@@ -54,7 +68,17 @@ assert(JSON.stringify(ipfsArray.map(i => i.path)) == JSON.stringify([
 console.log('compare outPathForTest[0] to expected', ipfsArray[0]);
 assert(JSON.stringify(ipfsArray[0]) == JSON.stringify({
     path: 'metadata/0000000000000000000000000000000000000000000000000000000000000001.json',
-    content: { image: `ipfs://${hash}/images/1.png`, name: '', description: '' }
+    content: {
+        image: 'ipfs://123/images/1.png',
+        cover_image: 'ipfs://123/images/1.png',
+        collection_cover_image: 'ipfs://123/images/cover.gif',
+        sprite: { image: 'ipfs://123/images/1.png', json: {} },
+        category: 'avatars',
+        name: '',
+        description: '',
+        background_color: '5E75C6',
+        attributes: {}
+    }
 }));
 
 
