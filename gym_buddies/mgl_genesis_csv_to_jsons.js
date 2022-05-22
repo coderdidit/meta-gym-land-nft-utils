6c8d449d7ef26108dd2e235a61629e54b85fe8bd
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-05-22 16:43:38 +0200

    feat: refine how we generate token metadata in gymbuddies nft

--- gym_buddies/mgl_genesis_csv_to_jsons.js
+++ gym_buddies/mgl_genesis_csv_to_jsons.js
@@ -7,32 +7,35 @@ const lines = csv.toString().split("\r");
 const properties = lines[0].split(",")
 const values = lines.slice(1);
 
-const collection_cid = "cid";
+const collection_cid = "QmPUQSULAxGXK321PMJKE5Qcs3xHvRuxDzDUjoB8g9cmzD";
+
+const runTest = false;
 
 for (const [lineIndex, lineValues] of values.entries()) {
-    if (lineIndex < 10) {
-        console.log(lineValues);
-        const attributes = [];
-        for (const [valIndex, lineValue] of lineValues.split(",").entries()) {
-            if (lineValue && properties[valIndex] != "number") {
-                const attr = {
-                    "trait_type": properties[valIndex],
-                    "value": lineValue.replace("\n", ""),
-                }
-                attributes.push(attr);
+    if (runTest && lineIndex > 10) break;
+    console.log(lineValues);
+    const attributes = [];
+    for (const [valIndex, lineValue] of lineValues.split(",").entries()) {
+        if (lineValue && properties[valIndex] != "number") {
+            const attr = {
+                "trait_type": properties[valIndex],
+                "value": lineValue.replace("\n", ""),
             }
+            attributes.push(attr);
         }
-        const idx = lineIndex + 1;
-        const obj = {
-            "description": "MetaGymLand GymBuddy that loves AI and NFTs",
-            "image": `https://gateway.pinata.cloud/ipfs/${collection_cid}/gb${idx}.png`,
-            "sprite": `https://gateway.pinata.cloud/ipfs/${collection_cid}/gbpx${idx}.png`,
-            "collection_name": "MetaGymLand Genesis GymBuddies",
-            "attributes": attributes,
-        };
-        console.log(obj);
-        // save as json file
-        const json = JSON.stringify(obj);
-        fs.writeFileSync(`mgl_genesis_jsons/gb${idx}.json`, json);
     }
+    const idx = lineIndex + 1;
+    const nameObj = attributes.find(a => a["trait_type"] == "name");
+    const obj = {
+        "name": nameObj ? nameObj["value"] : "noname",
+        "description": "MetaGymLand GymBuddy that loves AI and NFTs",
+        "image": `https://gateway.pinata.cloud/ipfs/${collection_cid}/gb${idx}.png`,
+        "sprite": `https://gateway.pinata.cloud/ipfs/${collection_cid}/gbpx${idx}.png`,
+        "collection_name": "MetaGymLand Genesis GymBuddies",
+        "attributes": attributes,
+    };
+    if (runTest) console.log(obj);
+    // save as json file
+    const json = JSON.stringify(obj);
+    fs.writeFileSync(`mgl_genesis_jsons/gb${idx}.json`, json);
 }
