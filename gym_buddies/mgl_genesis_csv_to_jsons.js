de2cfc0b6165527484714cdedb2bb80f0a710582
mkuzdowicz <martin.kuzdowicz@gmail.com>
2022-05-26 22:36:31 +0200

    feat: add gamefi attributes

--- gym_buddies/mgl_genesis_csv_to_jsons.js
+++ gym_buddies/mgl_genesis_csv_to_jsons.js
@@ -37,6 +37,35 @@ const resoveLevel = (idx) => {
     return "common"
 };
 
+const getGameFiRandomAttirbutes = () => {
+    return [
+        {
+            "trait_type": "endurance",
+            "value": 1 + (Math.random() * 10 ** 18) % 70,
+        },
+        {
+            "trait_type": "consistency",
+            "value": 1 + (Math.random() * 10 ** 18) % 40,
+        },
+        {
+            "trait_type": "sweatiness",
+            "value": 5 + (Math.random() * 10 ** 18) % 100,
+        },
+        {
+            "trait_type": "motivation",
+            "value": 1 + (Math.random() * 10 ** 18) % 80,
+        },
+        {
+            "trait_type": "strength",
+            "value": 1 + (Math.random() * 10 ** 18) % 50,
+        },
+        {
+            "trait_type": "luck",
+            "value": 1 + (Math.random() * 10 ** 18) % 100,
+        }
+    ]
+}
+
 for (const [lineIndex, lineValues] of values.entries()) {
     if (runTest && lineIndex > 10) break;
     console.log(lineValues);
@@ -55,6 +84,8 @@ for (const [lineIndex, lineValues] of values.entries()) {
         "trait_type": "level",
         "value": resoveLevel(lineIndex),
     });
+    // gameFi
+    attributes.push(...getGameFiRandomAttirbutes());
     const idx = lineIndex + 1;
     const nameObj = attributes.find(a => a["trait_type"] == "name");
     const obj = {
